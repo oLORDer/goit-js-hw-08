a29b335defb8d149597b694c6733422379831ed2
oLORDer <justsendmeow@gmail.com>
2022-07-07 23:52:32 +0300

    ff

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+
+const galerry = document.querySelector('.gallery');
+
+galerry.insertAdjacentHTML('beforeend', createImg(galleryItems));
+
+function createImg(arr) {
+  return arr
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href=${original}>
+        <img class="gallery__image"
+        src=${preview}
+        alt=${description} />
+    </a>
+    `;
+    })
+    .join('');
+}
+
+const lightboxGallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
 
 console.log(galleryItems);
