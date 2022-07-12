6a99eb6a04edfe89cdda93bddad341c265c9f607
oLORDer <justsendmeow@gmail.com>
2022-07-12 15:59:49 +0300

    form

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,13 +1,11 @@
 import throttle from 'lodash.throttle';
-import LSApi from './localstorage';
 
 const LS_KEY = 'feedback-form-state';
 const formEl  = document.querySelector('.feedback-form');
 const userData = {};
 
-
 const checkLS = () => {
-  const dataFormLS = LSApi.load(LS_KEY);
+  const dataFormLS = localStorage.getItem(LS_KEY) === null ? undefined : JSON.parse(localStorage.getItem(LS_KEY));
 
   if (!dataFormLS) return;
 
@@ -21,14 +19,14 @@ const checkLS = () => {
 
 const onFormChange = (e) => {
   userData[e.target.name] = e.target.value;
-  LSApi.save(LS_KEY, userData);
+  localStorage.setItem(LS_KEY, JSON.stringify(userData));
 };
 
 const onFormSubmit = (e) => {
   e.preventDefault();
 
   console.log(userData);
-  LSApi.remove(LS_KEY);
+  localStorage.removeItem(LS_KEY);
   e.currentTarget.reset();
 }
 
@@ -36,34 +34,3 @@ formEl.addEventListener('input', throttle(onFormChange, 500));
 formEl.addEventListener('submit', throttle(onFormSubmit, 500));
 
 checkLS();
-
-// just@faf.ewf
-// const fillFormFields = () => {
-//   const formLS = LSApi.load(LS_KEY);
-
-//   if (!formLS) return;
-
-//   for (const key in formLS) {
-//     if (formLS.hasOwnProperty(key)) {
-//       formEl.elements[key].value = formLS[key];
-//     }
-//   }
-// }
-
-// const onFormElChange = e => {
-//   userData[e.target.name] = e.target.value;
-
-//   LSApi.save(LS_KEY, userData);
-// };
-
-// const onContactFormSubmit = e => {
-//   e.preventDefault();
-
-//   LSApi.remove(LS_KEY);
-//   e.currentTarget.reset();
-// };
-
-// formEl.addEventListener('input', throttle(onFormElChange, 500));
-// formEl.addEventListener('submit', throttle(onContactFormSubmit, 500));
-
-// fillFormFields();
--- src/js/localstorage.js
+++ src/js/localstorage.js
@@ -31,3 +31,44 @@ export default {
   load,
   remove,
 };
+
+// form to LS
+
+// import throttle from 'lodash.throttle';
+// import LSApi from './localstorage';
+
+// const LS_KEY = 'feedback-form-state';
+// const formEl  = document.querySelector('.feedback-form');
+// const userData = {};
+
+
+// const checkLS = () => {
+//   const dataFormLS = LSApi.load(LS_KEY);
+
+//   if (!dataFormLS) return;
+
+//   for (const key in dataFormLS) {
+//     if(dataFormLS.hasOwnProperty(key)) {
+//       formEl.elements[key].value = dataFormLS[key];
+//       userData[key] = dataFormLS[key];
+//     }
+//   }
+// }
+
+// const onFormChange = (e) => {
+//   userData[e.target.name] = e.target.value;
+//   LSApi.save(LS_KEY, userData);
+// };
+
+// const onFormSubmit = (e) => {
+//   e.preventDefault();
+
+//   console.log(userData);
+//   LSApi.remove(LS_KEY);
+//   e.currentTarget.reset();
+// }
+
+// formEl.addEventListener('input', throttle(onFormChange, 500));
+// formEl.addEventListener('submit', throttle(onFormSubmit, 500));
+
+// checkLS();
